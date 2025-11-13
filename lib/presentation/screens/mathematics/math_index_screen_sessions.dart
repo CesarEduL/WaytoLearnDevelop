b609c531258ca8d3cf361e0c7146a10e7aee9201
Andy Chiroque <andyalessandrochiroqueadanaque@gmail.com>
2025-11-13 12:12:20 -0500

    Add communication icon and interaction to MathIndexScreenSessions; implement dynamic loading and navigation to SessionProgressScreen.

--- lib/presentation/screens/mathematics/math_index_screen_sessions.dart
+++ lib/presentation/screens/mathematics/math_index_screen_sessions.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:waytolearn/core/services/orientation_service.dart';
+import 'package:waytolearn/presentation/screens/communication/session_progress_screen.dart';
 
 class MathIndexScreenSessions extends StatefulWidget {
   const MathIndexScreenSessions({super.key});
@@ -12,6 +14,9 @@ class MathIndexScreenSessions extends StatefulWidget {
 class _MathIndexScreenSessionsState extends State<MathIndexScreenSessions> {
   static final Future<String> _homeIconFuture = Future.value(
     'https://firebasestorage.googleapis.com/v0/b/waytolearn-3ebca.appspot.com/o/mathematics_resources%2Fhome-icon.svg?alt=media&token=38f6113e-7be1-4f7d-8490-c08f8a83abfa',
+  );
+  static final Future<String> _communicationIconFuture = Future.value(
+    'https://firebasestorage.googleapis.com/v0/b/waytolearn-3ebca.appspot.com/o/mathematics_resources%2Fcomunication-icon-switch.svg?alt=media&token=b014d9af-ee54-4e4b-bdc9-6f077ca4226a',
   );
 
   @override
@@ -28,6 +33,10 @@ class _MathIndexScreenSessionsState extends State<MathIndexScreenSessions> {
 
   @override
   Widget build(BuildContext context) {
+    final mediaSize = MediaQuery.of(context).size;
+    const designWidth = 912.0;
+    final scale = mediaSize.width / designWidth;
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: Stack(
@@ -106,9 +115,95 @@ class _MathIndexScreenSessionsState extends State<MathIndexScreenSessions> {
                 ),
               ),
             ),
+            Positioned(
+              top: 14 * scale,
+              left: 806 * scale,
+              child: SizedBox(
+                width: 83.33 * scale,
+                height: 40 * scale,
+                child: Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Positioned.fill(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF5CF6D7),
+                          borderRadius: BorderRadius.circular(20 * scale),
+                          boxShadow: [
+                            BoxShadow(
+                              color: const Color(0x8000C7AF),
+                              offset: Offset(0, 6 * scale),
+                              blurRadius: 12 * scale,
+                            ),
+                            BoxShadow(
+                              color: const Color(0x4D00C7AF),
+                              offset: Offset(0, 2 * scale),
+                              blurRadius: 6 * scale,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      left: (862 - 856) * scale,
+                      top: (27 - 24) * scale,
+                      child: GestureDetector(
+                        onTap: _openCommunication,
+                        child: SizedBox(
+                          width: 36 * scale,
+                          height: 33 * scale,
+                          child: FutureBuilder<String>(
+                            future: _communicationIconFuture,
+                            builder: (context, snapshot) {
+                              final isReady = snapshot.connectionState == ConnectionState.done && snapshot.hasData;
+                              return Container(
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFF9C74F2),
+                                  borderRadius: BorderRadius.circular(20 * scale),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: const Color(0x66000000),
+                                      offset: Offset(0, 4 * scale),
+                                      blurRadius: 6 * scale,
+                                    ),
+                                  ],
+                                ),
+                                alignment: Alignment.center,
+                                child: isReady
+                                    ? SvgPicture.network(
+                                        snapshot.data!,
+                                        fit: BoxFit.contain,
+                                        placeholderBuilder: (context) => const SizedBox.shrink(),
+                                      )
+                                    : Icon(
+                                        Icons.chat_bubble,
+                                        size: 18 * scale,
+                                        color: Colors.white,
+                                      ),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
 
           ],
         ),
+    );
+  }
+
+  Future<void> _openCommunication() async {
+    await OrientationService().setLandscapeOnly();
+    if (!mounted) return;
+    await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => const SessionProgressScreen(),
+      ),
     );
   }
 }
